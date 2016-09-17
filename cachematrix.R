8ed1d6cbd0deb44c3db518c31fca29927ff69828
Fredagsilva <fredericoagsilva@gmail.com>
2016-09-17 13:17:49 -0300

    My solution in cachematrix.R
    My solution of Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+#That functions will make the inverse of a matrix, but to save some memory and make the
+#process fast, the function will verify if there is the answer in cache.
 
-## Write a short comment describing this function
+#This function create a space to cache a matrix, so you must to create a matrix and then
+#cache that in makeCacheMatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+#This function makes the inverse of a matrix, but first check if the answer is in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
